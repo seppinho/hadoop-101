98ec3f1343021c90c5c01cd72a60b5d019a0bbde
lukfor <lukas.forer@i-med.ac.at>
2014-10-08 16:27:43 +0200

    Completed tutorial.

---
+++ Examples/src/wordcount/WordCountJob.java
@@ -0,0 +1,30 @@
+package wordcount;
+
+import genepi.hadoop.HadoopJob;
+
+import org.apache.commons.logging.Log;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+
+public class WordCountJob extends HadoopJob {
+
+	public WordCountJob() {
+		super("Word Count");
+	}
+	
+	public WordCountJob(Log log) {
+		super("Word Count", log);
+	}
+
+	@Override
+	public void setupJob(Job job) {
+		job.setJarByClass(WordCountJob.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+	}
+
+}
---
+++ Examples/src/wordcount/WordCountStep.java
@@ -0,0 +1,43 @@
+package wordcount;
+
+import genepi.hadoop.common.ContextLog;
+import genepi.hadoop.common.StepRunner;
+import genepi.hadoop.common.WorkflowContext;
+import genepi.hadoop.common.WorkflowStep;
+
+public class WordCountStep extends WorkflowStep {
+
+	public boolean run(WorkflowContext context) {
+
+		context.beginTask("Running WordCount...");
+
+		String input = context.get("input");
+		String output = context.get("output");
+
+		WordCountJob job = new WordCountJob(new ContextLog(context));
+		job.setInput(input);
+		job.setOutput(output);
+
+		if (job.execute()) {
+			context.endTask("Running WordCount successful.", WorkflowContext.OK);
+			return true;
+		} else {
+			context.endTask("Running WordCount failed.", WorkflowContext.ERROR);
+			return false;
+
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		boolean result = StepRunner.run(args, new WordCountStep());
+
+		if (result) {
+			System.exit(0);
+		} else {
+			System.exit(1);
+		}
+
+	}
+
+}
